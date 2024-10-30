1b5543463ae10d1f6ea553245b6c03d1b97ebcca
Justas <justas.narijauskas@gmail.com>
2024-10-30 21:53:47 +0100

    stebuklingas zaidimas

--- 11-REACT/project-stebuklingas-zaidimas/src/App.js
+++ 11-REACT/project-stebuklingas-zaidimas/src/App.js
@@ -1,31 +1,63 @@
 import './App.css';
-import { useState } from 'react';
-import Korta from './components/korta';
-
+import { useEffect, useState } from 'react';
+import Korta from './components/Korta';
+ 
 const kortuImg = [
-  { 'src': 'img/pic-1.jpg', id: 1 },
-  { 'src': 'img/pic-2.jpg', id: 2 },
-  { 'src': 'img/pic-3.jpg', id: 3 },
-  { 'src': 'img/pic-4.jpg', id: 4 },
-  { 'src': 'img/pic-5.png', id: 5 },
-  { 'src': 'img/pic-6.png', id: 6 }
+  { 'src': 'img/pic-1.jpg', matched:false},
+  { 'src': 'img/pic-2.jpg', matched:false},
+  { 'src': 'img/pic-3.jpg', matched:false},
+  { 'src': 'img/pic-4.jpg', matched:false},
+  { 'src': 'img/pic-5.png', matched:false},
+  { 'src': 'img/pic-6.png', matched:false}
 ];
 
 function App() {
   const [kortos, setKortos] = useState([]);
   const [bandymai, setBandymai] = useState(0);
+  const [disabled, setDisabled] = useState(false)
   const manoKortos = () => {
     const visosKortos = [...kortuImg, ...kortuImg].sort(() => Math.random() - 0.5).map((korta) => ({ ...korta, id: Math.random() }));
     setKortos(visosKortos);
     setBandymai(0);
   };
 
+  const resetBandymus = () => {
+    setPasirinkimas1(null)
+    setPasirinkimas2(null)
+    setBandymai(anksciau => anksciau + 1)
+    setDisabled(false)
+  }
+
 const [pasirinkimas1, setPasirinkimas1] = useState(null)
 const [pasirinkimas2, setPasirinkimas2] = useState(null)
+
+useEffect(() => {
+  if(pasirinkimas1 && pasirinkimas2) {
+    setDisabled(true)
+    if(pasirinkimas1.src === pasirinkimas2.src){
+      setKortos(prevKortos => {
+        return prevKortos.map(korta => {
+          if(korta.src === pasirinkimas1.src) {
+            return {...korta, matched:true}
+          } else {
+            return korta
+          }
+        })
+      })
+      resetBandymus()
+    } else {
+      setTimeout(() => resetBandymus(), 1000)
+    }
+  }
+}, [pasirinkimas1, pasirinkimas2])
 
 const handlePasirinkimas = (korta) => {
   pasirinkimas1 ? setPasirinkimas2(korta) : setPasirinkimas1(korta)
 }
+
+useEffect(() => {
+  manoKortos()
+}, [])
 
   return (
     <div className="App">
@@ -39,10 +71,13 @@ const handlePasirinkimas = (korta) => {
             key={korta.id}
             korta={korta}
             handlePasirinkimas={handlePasirinkimas}
+            flipped={korta === pasirinkimas1 || korta === pasirinkimas2 || korta.matched}
+            disabled={disabled}
           />
         ))
         }
       </div>
+      <p>Bandymai: {bandymai}</p>
     </div>
   );
 }
--- 11-REACT/project-stebuklingas-zaidimas/src/components/korta.js
+++ 11-REACT/project-stebuklingas-zaidimas/src/components/korta.js
@@ -1,13 +1,15 @@
-import './korta.css'
+import './Korta.css'
 
-export default function Korta ({korta, handlePasirinkimas}) {
+export default function Korta ({korta, handlePasirinkimas, flipped, disabled}) {
     const handleClick = () => {
+      if(!disabled) {
         handlePasirinkimas(korta)
+      } 
     }
 
     return (
         <div className="korta">
-            <div>
+            <div className={flipped ? 'flipped' : ''}>
               <img src={korta.src} alt="kortos priekine puse" className="priekis" />
               <img src="img/cover.webp" alt="kortos nugarine puse" className="uzdengta" onClick={handleClick} />
             </div>
